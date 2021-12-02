6ef0d9093eac0f383f16258261d5d68f2a4a626c
MytsV <mytsvictoria@gmail.com>
2021-12-02 19:15:27 +0200

    Module finished

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const result = (idx) => arr[idx];
+  result.push = (element) => arr.push(element);
+  result.pop = () => arr.pop();
+  return result;
+};
 
 module.exports = { array };
